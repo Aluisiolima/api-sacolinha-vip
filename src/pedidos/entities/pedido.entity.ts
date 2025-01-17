e3e480a465ffc73836cbc28e66f661eba6c63362
Aluisio Lima <aaluisio309@gmail.com>
2025-01-17 20:57:56 -0300

    feat(Pedidos): criada a entidade pedido

--- src/pedidos/entities/pedido.entity.ts
+++ src/pedidos/entities/pedido.entity.ts
@@ -1,1 +1,25 @@
-export class Pedido {}
+import { Empresa } from "src/empresa/entities/empresa.entity"
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm"
+
+@Entity("pedidos")
+export class Pedido {
+
+    @PrimaryGeneratedColumn({name:"id_pedido"})
+    id:number
+
+    @Column({name:"nome_cliente", type:"varchar", length:100, nullable:false})
+    nome:string
+
+    @Column({type:"varchar", length:200, nullable:true})
+    endereco:string | null
+
+    @Column({type:"varchar", length:20, nullable:false})
+    numero_contato:string
+
+    @Column({type:"varchar", length:20, nullable:false})
+    status:string
+
+    @ManyToOne(() => Empresa, (empresa) => empresa.pedido, {onDelete:"CASCADE"})
+    @JoinColumn({name:"id_empresa"})
+    empresa:Empresa
+}
