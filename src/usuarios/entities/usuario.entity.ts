715ae5b73bc2b70752d83bacda4c9d23488a0a14
Aluisio Lima <aaluisio309@gmail.com>
2025-01-17 21:47:58 -0300

    feat(Usuarios): criado rotas simples e criado a entidade de banco dos usuarios

---
+++ src/usuarios/entities/usuario.entity.ts
@@ -0,0 +1,21 @@
+import { Empresa } from "src/empresa/entities/empresa.entity"
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm"
+
+@Entity("usuarios")
+export class Usuario {
+    @PrimaryGeneratedColumn({ name:"id_usuario" })
+    id:number
+
+    @Column({type:"varchar", length:100, nullable:false})
+    nome:string
+
+    @Column({type:"varchar", length:200, nullable:false})
+    senha:string
+
+    @Column({type:"varchar", length:20, nullable:false})
+    cargo:string
+
+    @ManyToOne(() => Empresa, (empresa) => empresa.usuarios, {onDelete:"CASCADE"})
+    @JoinColumn({ name:"id_empresa"} )
+    empresa:Empresa
+}
